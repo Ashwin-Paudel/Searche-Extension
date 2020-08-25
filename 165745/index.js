5b2f666ba237e393b1dda53f292e4de676065e58
Ashwin Paudel <62433766+Ashwin-Paudel@users.noreply.github.com>
2020-08-25 16:52:59 -0400

    Update index.js

--- 165745/index.js
+++ 165745/index.js
@@ -1,9 +1,1 @@
 alert('hello world');
-<menu type="context" id="mymenu">
-    <menuitem label="Refresh Post" onclick="window.location.reload();" icon="/images/refresh-icon.png"></menuitem>
-    <menuitem label="Skip to Comments" onclick="window.location='#comments';" icon="/images/comment_icon.gif"></menuitem>
-    <menu label="Share on..." icon="/images/share_icon.gif">
-        <menuitem label="Twitter" icon="/images/twitter_icon.gif" onclick="goTo('//twitter.com/intent/tweet?text=' + document.title + ':  ' + window.location.href);"></menuitem>
-        <menuitem label="Facebook" icon="/images/facebook_icon16x16.gif" onclick="goTo('//facebook.com/sharer/sharer.php?u=' + window.location.href);"></menuitem>
-    </menu>
-</menu>
