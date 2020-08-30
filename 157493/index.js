6bb26ed21f8b0b715a15fb4e5f1dca6a602d7684
Ashwin Paudel <62433766+Ashwin-Paudel@users.noreply.github.com>
2020-08-30 16:51:38 -0400

    Update index.js

--- 157493/index.js
+++ 157493/index.js
@@ -1,1 +1,11 @@
-
+function leftTapped() {
+    alert('Left');
+}
+function rightTapped() {
+    alert('Right');
+}
+function changeImagePathWith(path){
+    //"http://images.clipartpanda.com/smiley-face-png-1407-smiley-face.png"
+    document.getElementById("img").src=path;
+}
+window.onload=function () { window.webkit.messageHandlers.sizeNotification.postMessage({width: document.width, height: document.body.scrollHeight});};
