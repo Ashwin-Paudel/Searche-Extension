d6b7d4f58d583c537041f29cab963199d918b61a
Ashwin Paudel <62433766+Ashwin-Paudel@users.noreply.github.com>
2020-09-12 15:34:28 -0400

    Update index.js

--- 193840/index.js
+++ 193840/index.js
@@ -1,7 +1,8 @@
 function enableDark () {
 var styleTag = document.createElement("style");
-styleTag.textContent = '@media (prefers-color-scheme:dark){html{-webkit-filter:invert(100%)!important}html,iframe{-webkit-filter:invert(100%)!important}audio,button,em,embed,figure:empty,form,img,object,svg,video{opacity:.8;-webkit-filter:invert(100%)!important}input,textarea{-webkit-filter:invert(25%)!important}nav{-webkit-filter:invert(15%)!important}header{-webkit-filter:invert(100%)!important}}';
+styleTag.textContent = 'html{-webkit-filter:invert(100%)!important}html,iframe{-webkit-filter:invert(100%)!important}audio,button,em,embed,figure:empty,form,img,object,svg,video{opacity:.8;-webkit-filter:invert(100%)!important}input,textarea{-webkit-filter:invert(25%)!important}nav{-webkit-filter:invert(15%)!important}header{-webkit-filter:invert(100%)!important}';
 document.documentElement.appendChild(styleTag);
+  window.location.reload(true);
 }
 function enableLight () {
 var styleTag = document.createElement("style");
