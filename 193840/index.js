a2b414e23bc20c831576560cb3d9a25cbf4e3bc8
Ashwin Paudel <62433766+Ashwin-Paudel@users.noreply.github.com>
2020-09-12 15:40:06 -0400

    Update index.js

--- 193840/index.js
+++ 193840/index.js
@@ -1,11 +1,4 @@
-function enableDark() {
 var styleTag = document.createElement("style");
 styleTag.textContent = 'html{-webkit-filter:invert(100%)!important}html,iframe{-webkit-filter:invert(100%)!important}audio,button,em,embed,figure:empty,form,img,object,svg,video{opacity:.8;-webkit-filter:invert(100%)!important}input,textarea{-webkit-filter:invert(25%)!important}nav{-webkit-filter:invert(15%)!important}header{-webkit-filter:invert(100%)!important}';
 document.documentElement.appendChild(styleTag);
-  window.location.reload(true);
-}
-function enableLight() {
-var styleTag = document.createElement("style");
-  styleTag.textContent = 'html{-webkit-filter:invert(100%)!important}html,iframe{-webkit-filter:invert(100%)!important}audio,button,em,embed,figure:empty,form,img,object,svg,video{opacity:.8;-webkit-filter:invert(100%)!important}input,textarea{-webkit-filter:invert(25%)!important}nav{-webkit-filter:invert(15%)!important}header{-webkit-filter:invert(100%)!important}';
-document.documentElement.appendChild(styleTag);
-}
+window.location.reload(true);
