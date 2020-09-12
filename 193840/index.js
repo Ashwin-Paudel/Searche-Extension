76fcf6c8e3715d6e927eb509fabcd69e2d462bd3
Ashwin Paudel <62433766+Ashwin-Paudel@users.noreply.github.com>
2020-09-12 15:38:30 -0400

    Update index.js

--- 193840/index.js
+++ 193840/index.js
@@ -6,6 +6,6 @@ document.documentElement.appendChild(styleTag);
 }
 function enableLight() {
 var styleTag = document.createElement("style");
-styleTag.textContent = '';
+  styleTag.textContent = 'html{-webkit-filter:invert(100%)!important}html,iframe{-webkit-filter:invert(100%)!important}audio,button,em,embed,figure:empty,form,img,object,svg,video{opacity:.8;-webkit-filter:invert(100%)!important}input,textarea{-webkit-filter:invert(25%)!important}nav{-webkit-filter:invert(15%)!important}header{-webkit-filter:invert(100%)!important}';
 document.documentElement.appendChild(styleTag);
 }
